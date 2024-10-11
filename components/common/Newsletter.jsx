5fe56512405d2b23a73413da545fac8fd4f693f1
deniz19283 <142881082+deniz19283@users.noreply.github.com>
2024-10-11 20:54:05 +0200

    Update Newsletter.jsx

--- components/common/Newsletter.jsx
+++ components/common/Newsletter.jsx
@@ -4,11 +4,11 @@ export default function Newsletter() {
   return (
     <div className='flex flex-col gap-12 newsletter'>
       <h4 className='text-3xl text-[#afafaf] font-bold my-4'>
-        Anmäl dig till vårt nyhetsbrev
+        Register for the newsletter
       </h4>
       <form className='flex  gap-6'>
-        <input type='email' placeholder='Din e-postadress' className=' w-full p-2 pl-8 text-[16px] text-[#000] outline-none rounded-[100px]' />
-        <button type='submit' className='button !bg-white !text-[#000]'>Anmäl mig</button>
+        <input type='email' placeholder='Your email' className=' w-full p-2 pl-8 text-[16px] text-[#000] outline-none rounded-[100px]' />
+        <button type='submit' className='button !bg-white !text-[#000]'>Yes I want the newsletter</button>
       </form>
     </div>
   );
